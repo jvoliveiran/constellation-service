b25000d488c4d0bb530392c9a8e5a64bd0726c5d
Joao Oliveira <jvoliveiran@gmail.com>
2024-03-28 15:56:55 -0300

    chore: add graphql federated module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: true,
     }),
   ],
   controllers: [],
