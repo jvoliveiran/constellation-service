e6c16e6ca2e55ec54fdc460368e89a5841482523
Joao Oliveira <jvoliveiran@gmail.com>
2024-04-29 20:14:20 -0300

    chore: setup port via env var

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(
     new ValidationPipe({
       disableErrorMessages: false,
     }),
   );
-  await app.listen(3000);
+  const port = configService.get('SERVICE_PORT');
+  await app.listen(port || 3000);
 }
 bootstrap();
