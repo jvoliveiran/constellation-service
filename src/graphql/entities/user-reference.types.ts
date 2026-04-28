d848099e09508fa08ebc5843e65042bba5d6545a
João Oliveira <jvoliveiran@gmail.com>
2026-04-27 23:41:11 -0300

    refactor: remove unnecessary @extends directive from user reference

--- src/graphql/entities/user-reference.types.ts
+++ src/graphql/entities/user-reference.types.ts
@@ -4,7 +4,6 @@ import { ObjectType, Field, ID, Directive } from '@nestjs/graphql';
   description: 'Federated User entity reference. Owned by user-service.',
 })
 @Directive('@key(fields: "id")')
-@Directive('@extends')
 export class UserReference {
   @Field(() => ID, { description: 'Unique user identifier.' })
   @Directive('@external')
